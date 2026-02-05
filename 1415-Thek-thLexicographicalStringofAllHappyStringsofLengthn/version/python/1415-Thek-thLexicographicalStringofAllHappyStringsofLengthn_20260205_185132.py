cb7ed7e5ee281020a7ce4b72b48683a8d1f3ee1b
Nagarjuna A J <159552544+cyb3rarjun10@users.noreply.github.com>
2026-02-05 18:51:33 +0530

    Create 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn

---
+++ 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn/version/python/1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn_20260205_185132.py
@@ -0,0 +1,19 @@
+# Last updated: 2/5/2026, 6:51:32 PM
+1class Solution:
+2    def getHappyString(self, n: int, k: int) -> str:
+3        happystr=[]
+4        def genhappystr(s):
+5            if len(s)==n:
+6                happystr.append(s)
+7                return
+8            for i in {"a","b","c"}:
+9                if s and s[-1]==i:
+10                    continue
+11                else:
+12                    genhappystr(s+i)
+13        genhappystr("")
+14        happystr.sort()
+15
+16        if len(happystr)<k:
+17            return ""
+18        return happystr[k-1]
