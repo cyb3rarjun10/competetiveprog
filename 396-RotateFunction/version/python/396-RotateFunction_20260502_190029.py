14556526be888788cf59d14f64e1a946bf56374e
Nagarjuna A J <159552544+cyb3rarjun10@users.noreply.github.com>
2026-05-02 19:00:29 +0530

    Create 396-RotateFunction

---
+++ 396-RotateFunction/version/python/396-RotateFunction_20260502_190029.py
@@ -0,0 +1,11 @@
+# Last updated: 5/2/2026, 7:00:29 PM
+1class Solution:
+2    def maxRotateFunction(self, nums: List[int]) -> int:
+3        f, n, numSum = 0, len(nums), sum(nums)
+4        for i, num in enumerate(nums):
+5            f += i * num
+6        res = f
+7        for i in range(n - 1, 0, -1):
+8            f = f + numSum - n * nums[i]
+9            res = max(res, f)
+10        return res
